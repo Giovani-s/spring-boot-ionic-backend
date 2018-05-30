687dd3975a5c40745c13f08c425c358a8b78e936
Giovani Sopran <giovanisopran@gmail.com>
2018-05-29 22:52:39 -0300

    Cliente, TipoCliente, telefones e enderecos

---
+++ src/main/java/br/com/gs/repositories/ClienteRepository.java
@@ -0,0 +1,11 @@
+package br.com.gs.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.gs.domain.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+
+}
